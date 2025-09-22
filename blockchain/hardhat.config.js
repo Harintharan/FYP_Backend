618c2b22f0a89ceae0fe740483e81ce5e0c222d5
Thayanan Thevarasa <thevarasathayanan@gmail.com>
2025-09-22 13:45:57 +0530

    fix : hardhat configuration updated

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,26 +1,23 @@
-// import "@nomicfoundation/hardhat-toolbox";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// export default {
-//   solidity: "0.8.20",
-//   networks: {
-//     sepolia: {
-//       url: process.env.SEPOLIA_RPC_URL,
-//       accounts: [process.env.PRIVATE_KEY],
-//     },
-//   },
-// };
-
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: __dirname + "/../.env" }); // go up one level
+require("dotenv").config({ path: __dirname + "/../.env" }); 
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL || "";
+const GANACHE_PRIVATE_KEY = process.env.GANACHE_PRIVATE_KEY || "";
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.24",
   networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    ganache: {
+      url: GANACHE_RPC_URL,
+      accounts: [
+        GANACHE_PRIVATE_KEY,
+      ],
+    },
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
